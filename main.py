6891fcd6da5a136e9bf19eb9086193703b07570e
Jeffrin James <jeffrinjamesworks@gmail.com>
2026-03-01 16:06:25 +0000

    Added NEW UPDATE for New Games Health, speed, game launch and return to menu

--- main.py
+++ main.py
@@ -44,6 +44,17 @@ quit_rect = quit_btn.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 140))
 # // Game resources for {Start Game}
 
 game_bg = pygame.image.load("resourcepack/nether.png").convert()
+game_bg = pygame.transform.scale(game_bg, (WIDTH, HEIGHT))
+
+steve = pygame.image.load("resourcepack/steve02.png").convert_alpha()
+steve = pygame.transform.scale(steve, (80, 80))
+steve_rect = steve.get_rect(center=(WIDTH // 2, HEIGHT - 120))
+
+mob_img = pygame.image.load("resourcepack/Warden02.png").convert_alpha()
+mob_img = pygame.transform.scale(mob_img, (70, 70))
+
+heart_img = pygame.image.load("resourcepack/heart02.png").convert_alpha()
+heart_img = pygame.transform.scale(heart_img, (40, 40))
 
 # -------- LOAD MUSIC --------
 pygame.mixer.music.load("resourcepack/miceonvenus.mp3")
@@ -51,6 +62,15 @@ pygame.mixer.music.play(-1)  # Loop forever
 
 # -------- GAME STATE --------
 game_state = "menu"
+player_speed = 8
+
+health = 3
+score = 0
+mob_speed = 5
+
+mobs = []
+
+font = pygame.font.SysFont("Minecraft", 36)
 
 # -------- GAME LOOP --------
 running = True
@@ -69,6 +89,17 @@ while running:
             if start_rect.collidepoint(event.pos):
                 print("Start Game Clicked")
                 game_state = "playing"
+                pygame.mixer.music.load("resourcepack/Alpha.mp3")
+                pygame.mixer.music.play(-1)
+
+                health = 3
+                score = 0
+                mobs.clear()
+                steve_rect.center = (WIDTH // 2, HEIGHT - 120)
+
+            elif quit_rect.collidepoint(event.pos):
+                pygame.quit()
+                sys.exit()
 
             if learn_rect.collidepoint(event.pos):
                 print("Learn Clicked")
@@ -78,6 +109,41 @@ while running:
                 print("Quit Clicked")
                 pygame.quit()
                 sys.exit()
+
+# NEW UPDATE FOR START GAME
+        if game_state == "playing":
+            
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                steve_rect.x -= player_speed
+            if keys[pygame.K_RIGHT]:
+                steve_rect.x += player_speed
+
+            steve_rect.x = max(0, min(WIDTH - steve_rect.width, steve_rect.x))
+
+            if random.randint(1, 30) == 1:
+                mob_rect = mob_img.get_rect()
+                mob_rect.x = random.randint(0, WIDTH - mob_rect.width)
+                mob_rect.y = -70
+                mobs.append(mob_rect)
+
+                for mob in mobs[:]:
+                    mob.y += mob_speed
+
+                    if mob.colliderect(steve_rect):
+                        health -= -1
+                        mobs.remove(mob)
+
+                    elif mob.y > HEIGHT:
+                        mobs.remove(mob)
+                        score += 1
+
+                mob_speed = 5 + score // 10
+
+                if health <= 0:
+                    game_state = "menu"
+                    pygame.mixer.music.load("resourcepack/miceonvenus.mp3")
+                    pygame.mixer.music.play(-1)
 
     # -------- DRAW --------
     if game_state == "menu":
